5912022cab0f134f6658eea93367006dcd6c5ccf
Lee Hoang <leehoang97@gmail.com>
2023-04-29 20:36:15 -0700

    Initial project skeleton

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! Rust Starship Crawler library
+//! 4/29/2023
+//! //! CS 510 Rust
+//! 
+//! Library code goes here
+//! 
+//! TBD
+//! 
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+//! Main file
+//! 4/28/2023
+//! CS 510 Rust
+//! 
+//! This file is used to execute the game.
+//! 
+//! Run it using the command `cargo run`.
+//! 
+
+fn main() {
+    println!("TBD")
+}
